0d7f787cb7d33b44d30d728dbe420beb49db1b74
Varoon Behramsha <varoonbehramsha@gmail.com>
2020-01-17 09:50:15 +1100

    Initial Commit

---
+++ Bit Trader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bit Trader
+//
+//  Created by Varoon Behramsha on 17/01/20.
+//  Copyright © 2020 Varoon Behramsha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
